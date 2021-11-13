7a7a2bbd28f85bca02f40a1fa6675442dbf6bf26
liyuii <1136588217@qq.com>
2021-11-13 20:50:55 +0800

    修改项目结构，不同的功能放在不同的package

--- src/main/java/com/web/MainApplication.java
+++ src/main/java/com/web/MainApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@MapperScan("com.web.LoginModule.dao")
+@MapperScan("com.web.*.dao")
 @EnableCaching
 public class MainApplication {
     public static void main(String[] args) {
